c1ce5ba4df1c12fd9a75e1bbf2c7950a77f08d8d
Sarah-Katz <Sarahkatz59@gmail.com>
2023-04-19 11:01:21 +0200

    Ajout lien rgpd footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,10 @@ import React from "react";
 export default function Footer() {
     return (
         <footer>
+            <a className="footer-rgpd" href="/">Mentions légales</a>
             <span className="footer-text">Retrouvez nous sur </span>
-            <a className="footer-link" href="https://www.facebook.com/LaTableDeChantal/" target="_blank" rel="noreferrer"><i className="fa-brands fa-facebook footer-icon"></i></a>
-            <a className="footer-link" href="https://www.facebook.com/LaTableDeChantal/" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram footer-icon"></i></a>
+            <a className="footer-link" href="https://www.facebook.com/cinamand/" target="_blank" rel="noreferrer"><i className="fa-brands fa-facebook footer-icon"></i></a>
+            <a className="footer-link" href="https://www.instagram.com/cinamand/" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram footer-icon"></i></a>
         </footer>
     )
 }
